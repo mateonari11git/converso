a9937a19da344dea20b37f76d5708ca55d0599a6
mateonari11git <mateonari11@gmail.com>
2026-05-12 10:52:47 -0500

    Correción callback

--- functions/api/callback.js
+++ functions/api/callback.js
@@ -1,67 +1,60 @@
-function renderBody(status, content) {
-  return `
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="utf-8">
-  <title>Autorizando...</title>
-</head>
-<body>
-  <script>
-    const receiveMessage = (message) => {
-      window.opener.postMessage(
-        'authorization:github:${status}:${JSON.stringify(content)}',
-        message.origin
-      );
-
-      window.removeEventListener("message", receiveMessage, false);
-      window.close();
-    };
-
-    window.addEventListener("message", receiveMessage, false);
-    window.opener.postMessage("authorizing:github", "*");
-  </script>
-</body>
-</html>
-`;
-}
-
 export async function onRequest(context) {
   const { request, env } = context;
+
   const url = new URL(request.url);
   const code = url.searchParams.get("code");
 
   if (!code) {
-    return new Response(
-      renderBody("error", { message: "No se recibió código de GitHub." }),
-      { headers: { "Content-Type": "text/html" } }
-    );
+    return new Response("No code provided", { status: 400 });
   }
 
-  const tokenResponse = await fetch("https://github.com/login/oauth/access_token", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "Accept": "application/json"
-    },
-    body: JSON.stringify({
-      client_id: env.GITHUB_CLIENT_ID,
-      client_secret: env.GITHUB_CLIENT_SECRET,
-      code
-    })
-  });
+  const tokenResponse = await fetch(
+    "https://github.com/login/oauth/access_token",
+    {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        client_id: env.GITHUB_CLIENT_ID,
+        client_secret: env.GITHUB_CLIENT_SECRET,
+        code,
+      }),
+    }
+  );
 
-  const tokenData = await tokenResponse.json();
+  const data = await tokenResponse.json();
 
-  if (tokenData.error) {
+  if (!data.access_token) {
     return new Response(
-      renderBody("error", tokenData),
-      { headers: { "Content-Type": "text/html" } }
+      `OAuth Error: ${JSON.stringify(data)}`,
+      { status: 500 }
     );
   }
 
   return new Response(
-    renderBody("success", { token: tokenData.access_token }),
-    { headers: { "Content-Type": "text/html" } }
+    `
+<!doctype html>
+<html>
+  <body>
+    <script>
+      window.opener.postMessage(
+        'authorization:github:success:${JSON.stringify({
+          token: data.access_token,
+        })}',
+        window.location.origin
+      );
+
+      window.close();
+    </script>
+  </body>
+</html>
+`,
+    {
+      headers: {
+        "Content-Type": "text/html",
+      },
+    }
   );
 }
